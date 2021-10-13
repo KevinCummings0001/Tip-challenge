e7944cf8ff31314302fe5ed3022ac4882a67ddea
Kevin <kevincummings0001@yahoo.com>
2021-10-13 16:01:34 -0400

    final push for tip calculator

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
-# hello-world
-first repository
-This is  new territory for me.  I am committed
-A little tricky but way managable.
+
+Kevin Cummings 1st python program
+Tip calculator program 
+
+How to use.
+input the amounts taken from the bill (using numbers only) and follow prompts to determine amounts owed by each diner.
+The amount will show ending in how much each diner is responsible for paying.
--- tip_cal.py
+++ tip_cal.py
@@ -1,1 +1,21 @@
-print('hello')
+#entered instructions for how to use tip calculator.
+print('\nEnter Numbers below to decide how to split a restaurant bill amoung friends.')
+
+
+#created variables for inputs asking user to enter numbers.
+total_bill = float(input('What is the total bill?: '))
+percentage_tip = int(input('What % tip would you like to give?: '))
+number_of_people = int(input('How many people are splitting the bill?: '))
+
+
+#this is how and where the math formulas calculated the results.
+tax = .10
+taxed_bill = (total_bill * tax) + total_bill
+gratuity = taxed_bill * (percentage_tip /100)
+grand_total = taxed_bill + gratuity 
+each_diner_pays = grand_total / number_of_people
+
+
+#Final print statement showing tip calculation and thanking diners for using,
+print(f'\nThank you for using our simple Tip calculator\nThe grand total is ${grand_total:,.2f}\nEach member of the party is responsible for paying ${each_diner_pays:,.2f}.')
+
